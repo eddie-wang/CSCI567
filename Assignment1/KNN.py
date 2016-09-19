06b53939401d95a362f525e162250769dd852d44
eddie <786830875@qq.com>
2016-09-19 15:02:40 -0700

    KNN

--- Assignment1/KNN.py
+++ Assignment1/KNN.py
@@ -7,8 +7,8 @@ class KNN:
 	def __init__(self,training_data_file,testing_data_file):
 		self.preprocess_data(training_data_file,testing_data_file)
 		#print self.training_data
-		self.distance_method=self.euclideanDistance  #by default the distance method is euclidean 
-		self.K=5 #by default the k value is 1
+		self.distance_method=self.manhattanDistance  #by default the distance method is euclidean 
+		self.K=3 #by default the k value is 1
 	def preprocess_data(self,training_data_file,testing_data_file):
 		self.training_data,self.testing_data=[],[]
 		with open(training_data_file) as training_file:
@@ -25,10 +25,10 @@ class KNN:
 		mean[-1]=0
 		dv=list(df.std())
 		dv[-1]=1
-		print mean
+		print dv
 		self.training_data=((np.array(self.training_data)-mean)/dv).tolist()
 		self.testing_data=((np.array(self.testing_data)-mean)/dv).tolist()
-		print self.testing_data
+		##print self.testing_data[0]
 	def euclideanDistance(self,instance1, instance2):
 		distance = 0
 		for x in range(len(instance1)-1):
@@ -57,20 +57,22 @@ class KNN:
 		distances=[]
 		for item in training_set:
 			d=self.distance_method(item,testing_instance)
-			distances.append((d,item[-1],item))
+			distances.append((d,item[-1]))
 		
 		return sorted(distances,key=operator.itemgetter(0))[0:self.K]
 	def classify(self,testing_instance,training_set=None):
 		if training_set==None : training_set=self.training_data
 		neighbor=self.nNearestNeighbor(training_set,testing_instance)
-		##print neighbor
+		print neighbor
 		votes={}
 		for item in neighbor:
 			if item[1] in votes:
 				votes[item[1]]+=1
 			else:
 				votes[item[1]]=1
+		##print votes
 		w=sorted(votes.iteritems(),key=operator.itemgetter(1),reverse=True)
+		print w
 		return w[0][0]
 	def test(self):
 		err=0.0
@@ -78,6 +80,7 @@ class KNN:
 			if self.classify(item)!=item[-1]:
 				err+=1
 				##print item
+			return "aa"
 		return err/len(self.testing_data)
 if __name__ == '__main__':
 	training_data_file=sys.argv[1]
